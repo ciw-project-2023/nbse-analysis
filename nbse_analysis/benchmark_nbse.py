2b847cc39fe712ee053e3dcc213b1739649e92e2
Niklas Paulicks <nPaulicks@gmail.com>
2024-01-30 17:51:35 +0100

    more pics and plots and data

--- nbse_analysis/benchmark_nbse.py
+++ nbse_analysis/benchmark_nbse.py
@@ -284,15 +284,15 @@ if __name__ == '__main__':
         # hier kickt das conf minimum adding ziemlich hart
         # Config(num_conformers=10, optimizer_fine=0.01, optimizer_coarse=0.5, assemblies=1),
         # Config(num_conformers=30, optimizer_fine=0.01, optimizer_coarse=0.5, assemblies=1),
-        # Config(num_conformers=60, optimizer_fine=0.01, optimizer_coarse=0.5, assemblies=1),
+        Config(num_conformers=60, optimizer_fine=0.01, optimizer_coarse=0.5, assemblies=1),
 
         # Config(num_conformers=10, optimizer_fine=0.01, optimizer_coarse=0.3),
         # Config(num_conformers=20, optimizer_fine=0.01, optimizer_coarse=0.3),
-        # Config(num_conformers=40, optimizer_fine=0.01, optimizer_coarse=0.3),  done
+        # Config(num_conformers=40, optimizer_fine=0.01, optimizer_coarse=0.3)
 
-        Config(num_conformers=20, optimizer_fine=0.05, optimizer_coarse=0.5),
-        Config(num_conformers=20, optimizer_fine=0.10, optimizer_coarse=0.5),
-        Config(num_conformers=20, optimizer_fine=0.20, optimizer_coarse=0.5),
+        # Config(num_conformers=20, optimizer_fine=0.05, optimizer_coarse=0.5),
+        # Config(num_conformers=20, optimizer_fine=0.10, optimizer_coarse=0.5),
+        # Config(num_conformers=20, optimizer_fine=0.20, optimizer_coarse=0.5),
     ]
 
     results_csv = None
@@ -305,7 +305,7 @@ if __name__ == '__main__':
         results_csv = open('benchmark_results.csv', 'a')
 
     for conf in confs:
-        for name in first_forty:
+        for name in ['4ajn']:
             print("running: {}".format(name))
 
             try:
